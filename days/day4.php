12c2d6479678cb10dfa9003bf42643cac802b0e8
Julien Verhaeghe <julien.verhaeghe@interaction-healthcare.com>
2016-12-06 13:35:46 +0100

    Day 5 : Part 1 & 2

--- days/day4.php
+++ days/day4.php
@@ -4,10 +4,11 @@
     //
 
     // We get the input from the site
-    $input                  = file_get_contents('inputs/day4.txt');
+    $input                      = file_get_contents('inputs/day4.txt');
 
-    $lines                  = explode("\n", $input);
-    $sectors_ids            = 0;
+    $lines                      = explode("\n", $input);
+    $sectors_ids                = 0;
+    $real_rooms_encrypted_names = [];
 
     foreach($lines as $line) {
         $array          = explode('-', $line); // We explode the array to have the encrypted name and sector id and checksum
@@ -18,9 +19,13 @@
         $sector_id      = $matches[1];
         $checksum       = $matches[2];
 
-        $encrypted_name = implode('', $array); // We get a full string of the encrypted name
+        $encrypted_name = implode(' ', $array); // We get a full string of the encrypted name
 
         foreach(str_split($encrypted_name) as $letter) {
+            if ($letter === ' ') {
+                continue;
+            }
+
             if (!isset($letters_count[$letter])) {
                 $letters_count[$letter] = 0; // initializing variable
             }
@@ -34,8 +39,32 @@
         $temp_checksum  = implode('', array_splice($letters, 0, 5)); // We only get the first five letters for the checksum to compare
 
         if ($temp_checksum === $checksum) {
-            $sectors_ids += $sector_id; // If it's not a decoy, we add the sector id
+            $real_rooms_encrypted_names[$encrypted_name] = $sector_id;
+            $sectors_ids                += $sector_id; // If it's not a decoy, we add the sector id
         }
     }
 
     echo "Part one : Sectors IDs sum is ".$sectors_ids;
+    echo "\n";
+
+    foreach($real_rooms_encrypted_names as $real_encrypted_room_name => $sector_id) {
+        $real_encrypted_room_name_chars         = str_split($real_encrypted_room_name);
+        $real_encrypted_room_name_chars_count   = count($real_encrypted_room_name_chars);
+        $decrypted_text                         = '';
+        $alphabet                               = range('a', 'z');
+        $flip                                   = array_flip($alphabet);
+
+        for ($i = 0; $i < $real_encrypted_room_name_chars_count; $i++) {
+            if ($real_encrypted_room_name_chars[$i] === ' ') {
+                $decrypted_text .= ' ';
+            } else {
+                $decrypted_text .= $alphabet[(26+$flip[$real_encrypted_room_name_chars[$i]]+$sector_id%26)%26];
+            }
+        }
+
+        if (strpos($decrypted_text, 'north') !== false) {
+            echo "Part two : Location is '".$decrypted_text."' with sector id ".$sector_id;
+            echo "\n";
+            break;
+        }
+    }
---
+++ days/day5.php
@@ -0,0 +1,56 @@
+<?php
+    //
+    // PART ONE
+    //
+
+    // We get the input from the site
+    $input      = "cxdnnyjw";
+    $password   = [];
+    $cpt        = 0;
+
+    while (count($password) !== 8) {
+        $to_encrypt = $input.$cpt;
+
+        $encrypted  = md5($to_encrypt);
+
+        if (substr($encrypted, 0, 5) === "00000") {
+            $password [] = $encrypted[5];
+
+            for ($i = 0; $i < 8; $i++) {
+                echo (isset($password[$i]) ? $password[$i] : "_");
+            }
+
+            echo "\n";
+        }
+
+        $cpt++;
+    }
+
+    echo "Part one : password is '".implode('', $password)."' ({$cpt} iterations).";
+    echo "\n";
+
+
+    $password   = [];
+    $cpt        = 0;
+
+    while (count($password) !== 8) {
+        $to_encrypt = $input.$cpt;
+
+        $encrypted  = md5($to_encrypt);
+
+        if (substr($encrypted, 0, 5) === "00000" && $encrypted[5] >= 0 && $encrypted[5] < 8 && is_numeric($encrypted[5]) && !isset($password[$encrypted[5]])) {
+            $password[$encrypted[5]] = $encrypted[6];
+
+            for ($i = 0; $i < 8; $i++) {
+                echo (isset($password[$i]) ? $password[$i] : "_");
+            }
+
+            echo "\n";
+        }
+
+        $cpt++;
+    }
+
+    ksort($password);
+
+    echo "Part two : password is '".implode('', $password)."' ({$cpt} iterations).";
