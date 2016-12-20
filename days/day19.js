31f25ed782ab813b2978e07b548ed1d02f505f84
Julien Verhaeghe <julien.verhaeghe@interaction-healthcare.com>
2016-12-20 10:26:59 +0100

    Part Two is wrong. Deleted it.

--- days/day19.js
+++ days/day19.js
@@ -20,33 +20,3 @@ while (elves.length > 1) {
 }
 
 console.log(elves);
-
-var elves       = [];
-
-for(var i = 1; i <= elfes_count; i++) {
-    elves.push({'elf' : i, 'gifts' : 1});
-}
-
-var position = 0;
-
-while (elves.length > 1) {
-    var steal_index = (Math.floor(elves.length / 2) + position) % elves.length;
-
-    elves[position].gifts += elves[steal_index].gifts;
-
-    delete elves[steal_index];
-
-    elves =     _.compact(elves);
-    
-    if (position === elves.length) {
-        position = 0;
-    } else {
-        position++;
-    }
-    
-    if (elves.length%1000 === 0) {
-        console.log(elves.length);
-    }
-}
-
-console.log(elves);
